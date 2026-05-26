cb89747095042e297b3a89a2af722568b9d65abd
HYC <yechanhwangahah@gmail.com>
2026-05-26 15:24:10 +0900

    complete REP09_02

--- src/REP09_02/Rep09Employee2.java
+++ src/REP09_02/Rep09Employee2.java
@@ -13,11 +13,19 @@ class Person {
 class Employee extends Person {
     String ssn;
     int salary;
-    Employee() {}
+    Employee() {
+    }
+
     Employee(String name, String ssn, int salary) {
         super(name);
         this.ssn = ssn;
         this.salary = salary;
+    }
+}
+
+class SSNLengthException extends Exception {
+    SSNLengthException(String ssn) {
+        super("SSN Error: '" + ssn + "' is not 8 characters long.");
     }
 }
 
@@ -30,16 +38,35 @@ public class Rep09Employee2 {
         int count = Integer.parseInt(s.nextLine());
 
         for (int i = 0; i < count; i++) {
-            System.out.println("\nEntering data for employee " + (i +1));
+            System.out.println('\n');
+            System.out.println("Entering data for employee " + (i + 1));
 
             System.out.print("Enter name: ");
             String name = s.nextLine();
 
-            System.out.print("Enter SSN (no dashes or spaces): ");
-            String ssn = s.nextLine();
-
-            System.out.print("Enter salary:");
-            int salary = Integer.parseInt(s.nextLine());
+            String ssn = "";
+            while (true) {
+                System.out.print("Enter SSN (no dashes or spaces): ");
+                ssn = s.nextLine();
+                try {
+                    if (ssn.length() != 8) throw new SSNLengthException(ssn);
+                    break;
+                } catch (SSNLengthException e) {
+                    System.out.println(e.getMessage());
+                    System.out.println("Please enter agin.");
+                }
+            }
+            int salary = 0;
+            while (true) {
+                System.out.print("Enter salary: ");
+                try {
+                    salary = Integer.parseInt(s.nextLine());
+                    break;
+                } catch (NumberFormatException e) {
+                    System.out.println("Salary Error: A non-numeric value was entered.");
+                    System.out.println("Please enter again.");
+                }
+            }
 
             employees[i] = new Employee(name, ssn, salary);
         }
