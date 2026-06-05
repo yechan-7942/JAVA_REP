2e32cf48c78394b81e2f28dbd5e95a15c8c424aa
HYC <yechanhwangahah@gmail.com>
2026-06-05 14:40:02 +0900

    REp11_01

--- src/REP11/Handshake1.java
+++ src/REP11/Handshake1.java
@@ -1,11 +1,24 @@
 package REP11;
+import java.util.*;
 
 public class Handshake1 {
 
-    static void main(String[] args) {
-        int n=5;
-        System.out.println("Total handshakes ofr" + n  "people" + handshake(n));
+    public static int handshake(int n){
+        if(n<=1){
+            return 0;
+        }
+        if( n ==2){
+            return 1;
+        }
+        return (n-1) + handshake(n-1);
     }
+
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("Total handshakes for " + n + " " +"people: " + handshake(n));
+    }
+
 
 
 }
